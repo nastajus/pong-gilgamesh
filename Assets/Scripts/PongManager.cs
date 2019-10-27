62ed1000608c78f793669278d06f798d0377f2be
Ian Nastajus <ian.nastajus@gmail.com>
2019-10-27 19:19:14 -0400

    scores work

--- Assets/Scripts/PongManager.cs
+++ Assets/Scripts/PongManager.cs
@@ -8,6 +8,11 @@ public class PongManager : TSEnvironment
     [Header("Pong Manager")]
     public Wall WallNorth;
     public Wall WallSouth;
+    public Paddle PaddleWest;
+    public Paddle PaddleEast;
+
+    [HideInInspector] public int Score1;
+    [HideInInspector] public int Score2;
 
     //boundary logic if ball went too far... 
     //award points
@@ -18,8 +23,13 @@ public class PongManager : TSEnvironment
     private Collider _colBall;
     private Collider _colWall1;
     private Collider _colWall2;
+    private Collider _paddle1;
+    private Collider _paddle2;
 
     private Bounds _tableBounds;
+    private Bounds _scoreBoundsWest;
+    private Bounds _scoreBoundsEast;
+
 
     void Awake()
     {
@@ -30,19 +40,44 @@ public class PongManager : TSEnvironment
         _colWall2 = WallSouth.GetComponent<Collider>();
         _tableBounds = GetTableBounds();
 
+        _paddle1 = PaddleWest.GetComponent<Collider>();
+        _paddle2 = PaddleEast.GetComponent<Collider>();
+
+        //var scoreRegion = new Vector3(_tableBounds.size.y, _tableBounds.size.y);
+        var scoreWestCenteredBesidePaddleWest = _tableBounds.center - new Vector3(_tableBounds.extents.x, 0) + _paddle1.bounds.center - new Vector3(_paddle1.bounds.extents.x, 0);
+        var scoreEastCenteredBesidePaddleEast = _tableBounds.center + new Vector3(_tableBounds.extents.x, 0) + _paddle2.bounds.center + new Vector3(_paddle2.bounds.extents.x, 0);
+
+        _scoreBoundsWest = new Bounds(scoreWestCenteredBesidePaddleWest, _tableBounds.size);
+        _scoreBoundsEast = new Bounds(scoreEastCenteredBesidePaddleEast, _tableBounds.size);
+
     }
 
     void FixedUpdate()
     {
+        if (_colBall.bounds.Intersects(_scoreBoundsWest))
+        {
+            Score2++;
+            Debug.Log("player 2 scores. now respawning...");
+            Destroy(_colBall.gameObject);
+            SpawnBall();
+        }
+
+        if (_colBall.bounds.Intersects(_scoreBoundsEast))
+        {
+            Score1++;
+            Debug.Log("player 1 scores. now respawning...");
+            Destroy(_colBall.gameObject);
+            SpawnBall();
+        }
+
         //when ball goes out of bounds, destroy it and respawn it.
         if (!_colBall.bounds.Intersects(_tableBounds))
         {
-            Destroy(_colBall.gameObject);
             Debug.Log("ball went out of bounds, destroyed it. now respawning...");
-
+            Destroy(_colBall.gameObject);
             SpawnBall();
-
         }
+
 
     }
 
