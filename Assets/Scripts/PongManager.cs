d618870f9f8deb015bae1f6b0b4099cf8b80023f
Ian Nastajus <ian.nastajus@gmail.com>
2019-10-27 17:16:47 -0400

    wall array

--- Assets/Scripts/PongManager.cs
+++ Assets/Scripts/PongManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PongManager : TSEnvironment
 {
+
+    [Header("Pong Manager")]
+    public Wall[] Walls = new Wall[2];
+
     // Start is called before the first frame update
     void Start()
     {
