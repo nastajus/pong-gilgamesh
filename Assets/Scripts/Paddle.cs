331e79def39957e48c468b6be6cd94cfc8b3f1f9
Ian Nastajus <ian.nastajus@gmail.com>
2019-10-27 23:06:30 -0400

    debugged finally inaccuracy drift to paddle bounds center
    thanks to community: https://forum.unity.com/threads/problem-bug-using-renderer-bounds-center.148539/#post-1021987

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -13,41 +13,85 @@ public class Paddle : MonoBehaviour
 
     private Rigidbody _rb;
     private Collider _colPaddle;
+    private Mesh _meshPaddle;
 
 
     private PongManager _manager;
     private Collider _colWall1;
     private Collider _colWall2;
 
+    private float northWallBottom;
+    private float southWallTop;
+
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _colPaddle = GetComponent<Collider>();
+        _meshPaddle = GetComponent<MeshFilter>().mesh;
 
         _manager = transform.parent.GetComponent<PongManager>();
         _colWall1 = _manager.WallNorth.GetComponent<Collider>();
         _colWall2 = _manager.WallSouth.GetComponent<Collider>();
+
+        northWallBottom = _colWall1.bounds.center.y - _colWall1.bounds.extents.y;
+        southWallTop = _colWall2.bounds.center.y + _colWall2.bounds.extents.y;
     }
+
+
+    GameObject cube2;
+    GameObject cube3;
 
     void FixedUpdate()
     {
+        if (Player == Players.Player2) return;
+
         float moveDir = Move();
+        
+        if (moveDir == 0) return;
         Vector3 moveDestMaybe = transform.position + new Vector3(0, moveDir * Speed);
 
-        var northWallBottom = _colWall1.bounds.center.y - _colWall1.bounds.extents.y;
-        var southWallTop = _colWall2.bounds.center.y + _colWall2.bounds.extents.y;
+        //var paddleTop = _colPaddle.bounds.center.y - _colPaddle.bounds.extents.y;
+        //var paddleBottom = _colPaddle.bounds.center.y + _colPaddle.bounds.extents.y;
 
-        var paddleTop = _colPaddle.bounds.center.y - _colPaddle.bounds.extents.y;
-        var paddleBottom = _colPaddle.bounds.center.y + _colPaddle.bounds.extents.y;
+        var paddleTop = _meshPaddle.bounds.center.y - _meshPaddle.bounds.extents.y;
+        var paddleBottom = _meshPaddle.bounds.center.y + _meshPaddle.bounds.extents.y;
+        //The renderer.bounds values are given in world space, which is why it changes as the object moves. If the need the bounds in the object's local space, you should use Mesh.bounds.
+
+
+
+        //debug code
+        Destroy(cube2);
+            cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var rb2 = cube2.AddComponent<Rigidbody>();
+            rb2.MovePosition(transform.position + new Vector3(0, paddleTop, -1));
+            cube2.transform.localScale = new Vector3(1, 0.1f, 1);
+
+            Destroy(cube3);
+            cube3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var rb3 = cube3.AddComponent<Rigidbody>();
+            rb3.MovePosition(transform.position + new Vector3(0, paddleBottom, -1));
+            cube3.transform.localScale = new Vector3(1, 0.1f, 1);
+
+        _rb.MovePosition(moveDestMaybe);
 
         //TODO: fix logic error, gets stuck..
+
         //if (moveDir > 0 && paddleTop + moveDestMaybe.y < northWallBottom  ||
         //    moveDir < 0 && paddleBottom + moveDestMaybe.y < southWallTop) 
-        {
-            _rb.MovePosition(moveDestMaybe);
-        }
-        //Debug.Log($"md: {moveDir}, nwb: {northWallBottom}, pt: {paddleTop}, mdm: {moveDestMaybe.y}");
+
+        //if (!(moveDir > 0 && paddleTop + moveDestMaybe.y < northWallBottom))
+        //{
+        //    _rb.MovePosition(moveDestMaybe);
+        //}
+        //if (!(moveDir < 0 && paddleBottom + moveDestMaybe.y < southWallTop))
+        //{
+        //    _rb.MovePosition(moveDestMaybe);
+        //}
+
+
+
+        //Debug.Log($"player: {Player}  md: {moveDir}, pt: {paddleTop}, mdm: {moveDestMaybe.y}, nwb: {northWallBottom}");
 
     }
 
