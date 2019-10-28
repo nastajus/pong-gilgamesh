6c648eca938b4bcaf8381afc397ba947e99aa6b5
Ian Nastajus <ian.nastajus@gmail.com>
2019-10-27 23:41:25 -0400

    paddles now stop correctly at wall edges
    demorgan's law, why u do this to me...

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -12,7 +12,6 @@ public class Paddle : MonoBehaviour
 
 
     private Rigidbody _rb;
-    private Collider _colPaddle;
     private Mesh _meshPaddle;
 
 
@@ -27,7 +26,6 @@ public class Paddle : MonoBehaviour
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
-        _colPaddle = GetComponent<Collider>();
         _meshPaddle = GetComponent<MeshFilter>().mesh;
 
         _manager = transform.parent.GetComponent<PongManager>();
@@ -36,63 +34,31 @@ public class Paddle : MonoBehaviour
 
         northWallBottom = _colWall1.bounds.center.y - _colWall1.bounds.extents.y;
         southWallTop = _colWall2.bounds.center.y + _colWall2.bounds.extents.y;
+
+        var cube4 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        var rb4 = cube4.AddComponent<Rigidbody>();
+        rb4.MovePosition(transform.position + new Vector3(0, northWallBottom, -1));
+        cube4.transform.localScale = new Vector3(1, 0.1f, 1);
+
     }
-
-
-    GameObject cube2;
-    GameObject cube3;
 
     void FixedUpdate()
     {
-        if (Player == Players.Player2) return;
-
         float moveDir = Move();
-        
         if (moveDir == 0) return;
+
         Vector3 moveDestMaybe = transform.position + new Vector3(0, moveDir * Speed);
 
-        //var paddleTop = _colPaddle.bounds.center.y - _colPaddle.bounds.extents.y;
-        //var paddleBottom = _colPaddle.bounds.center.y + _colPaddle.bounds.extents.y;
+        var paddleTop = _meshPaddle.bounds.center.y + _meshPaddle.bounds.extents.y;
+        var paddleBottom = _meshPaddle.bounds.center.y - _meshPaddle.bounds.extents.y;
 
-        var paddleTop = _meshPaddle.bounds.center.y - _meshPaddle.bounds.extents.y;
-        var paddleBottom = _meshPaddle.bounds.center.y + _meshPaddle.bounds.extents.y;
-        //The renderer.bounds values are given in world space, which is why it changes as the object moves. If the need the bounds in the object's local space, you should use Mesh.bounds.
-
-
-
-        //debug code
-        Destroy(cube2);
-            cube2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            var rb2 = cube2.AddComponent<Rigidbody>();
-            rb2.MovePosition(transform.position + new Vector3(0, paddleTop, -1));
-            cube2.transform.localScale = new Vector3(1, 0.1f, 1);
-
-            Destroy(cube3);
-            cube3 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            var rb3 = cube3.AddComponent<Rigidbody>();
-            rb3.MovePosition(transform.position + new Vector3(0, paddleBottom, -1));
-            cube3.transform.localScale = new Vector3(1, 0.1f, 1);
-
-        _rb.MovePosition(moveDestMaybe);
-
-        //TODO: fix logic error, gets stuck..
-
-        //if (moveDir > 0 && paddleTop + moveDestMaybe.y < northWallBottom  ||
-        //    moveDir < 0 && paddleBottom + moveDestMaybe.y < southWallTop) 
-
-        //if (!(moveDir > 0 && paddleTop + moveDestMaybe.y < northWallBottom))
-        //{
-        //    _rb.MovePosition(moveDestMaybe);
-        //}
-        //if (!(moveDir < 0 && paddleBottom + moveDestMaybe.y < southWallTop))
-        //{
-        //    _rb.MovePosition(moveDestMaybe);
-        //}
-
-
-
-        //Debug.Log($"player: {Player}  md: {moveDir}, pt: {paddleTop}, mdm: {moveDestMaybe.y}, nwb: {northWallBottom}");
-
+        if (
+                ((moveDir > 0) && !(paddleTop + moveDestMaybe.y > northWallBottom)) ||
+                ((moveDir < 0) && !(paddleBottom + moveDestMaybe.y < southWallTop))
+            )
+        {
+            _rb.MovePosition(moveDestMaybe);
+        }
     }
 
     private float Move()
