53b3ac5af5514cf2888c842bf535fda957fe2860
Ian Nastajus <ian.nastajus@gmail.com>
2019-10-27 23:40:33 -0400

    refactor reset paddle logic

--- Assets/Scripts/PongManager.cs
+++ Assets/Scripts/PongManager.cs
@@ -14,13 +14,15 @@ public class PongManager : TSEnvironment
     [HideInInspector] public int Score1;
     [HideInInspector] public int Score2;
 
+    //private Paddle _paddleWestCopy;
+    //private Paddle _paddleEastCopy;
     private Ball _ball;
     private Ball _ballCopy;
     private Collider _colBall;
     private Collider _colWall1;
     private Collider _colWall2;
-    private Collider _paddle1;
-    private Collider _paddle2;
+    private Collider _colPaddle1;
+    private Collider _colPaddle2;
 
     private Bounds _tableBounds;
     private Bounds _scoreBoundsWest;
@@ -35,19 +37,24 @@ public class PongManager : TSEnvironment
         _colWall2 = WallSouth.GetComponent<Collider>();
         _tableBounds = GetTableBounds();
 
-        _paddle1 = PaddleWest.GetComponent<Collider>();
-        _paddle2 = PaddleEast.GetComponent<Collider>();
+        _colPaddle1 = PaddleWest.GetComponent<Collider>();
+        _colPaddle2 = PaddleEast.GetComponent<Collider>();
 
-        var scoreWestCenteredBesidePaddleWest = _tableBounds.center - new Vector3(_tableBounds.extents.x, 0) + _paddle1.bounds.center - new Vector3(_paddle1.bounds.extents.x, 0);
-        var scoreEastCenteredBesidePaddleEast = _tableBounds.center + new Vector3(_tableBounds.extents.x, 0) + _paddle2.bounds.center + new Vector3(_paddle2.bounds.extents.x, 0);
+        var scoreWestCenteredBesidePaddleWest = _tableBounds.center - new Vector3(_tableBounds.extents.x, 0) + _colPaddle1.bounds.center - new Vector3(_colPaddle1.bounds.extents.x, 0);
+        var scoreEastCenteredBesidePaddleEast = _tableBounds.center + new Vector3(_tableBounds.extents.x, 0) + _colPaddle2.bounds.center + new Vector3(_colPaddle2.bounds.extents.x, 0);
 
         _scoreBoundsWest = new Bounds(scoreWestCenteredBesidePaddleWest, _tableBounds.size);
         _scoreBoundsEast = new Bounds(scoreEastCenteredBesidePaddleEast, _tableBounds.size);
 
-        //save copy of ball to keep re-instantiating
+        //save copies to re-instantiate
         _ball = GetComponentInChildren<Ball>();
         _ball.gameObject.SetActive(false);
-        SpawnBall();
+        ResetBall();
+
+        //PaddleWest.gameObject.SetActive(false);
+        //PaddleEast.gameObject.SetActive(false);
+        //ResetPaddles();
+
     }
 
     void FixedUpdate()
@@ -59,16 +66,18 @@ public class PongManager : TSEnvironment
             Debug.Log("player 1 scores!");
             Destroy(_ballCopy.gameObject);
             CheckWinner();
-            SpawnBall();
+            ResetBall();
+            ResetPaddles();
         }
 
         if (_colBall.bounds.Intersects(_scoreBoundsWest))
         {
             Score2++;
             Debug.Log("player 2 scores!");
-            CheckWinner();
             Destroy(_ballCopy.gameObject);
-            SpawnBall();
+            CheckWinner();
+            ResetBall();
+            ResetPaddles();
         }
 
         //fix out-of-bounds
@@ -77,7 +86,8 @@ public class PongManager : TSEnvironment
             //no points awarded
             Debug.Log("ball went out of bounds, destroyed it. now respawning...");
             Destroy(_ballCopy.gameObject);
-            SpawnBall();
+            ResetBall();
+            ResetPaddles();
         }
     }
 
@@ -101,13 +111,36 @@ public class PongManager : TSEnvironment
         return tableBounds;
     }
 
-    void SpawnBall()
+    void ResetBall()
     {
         _ballCopy = Instantiate<Ball>(_ball, Vector3.zero, Quaternion.identity);
         _ballCopy.gameObject.SetActive(true);
         _ballCopy.transform.parent = transform;
         _ballCopy.name = nameof(Ball);
         _colBall = _ballCopy.GetComponent<Collider>();
+    }
+
+    void ResetPaddles()
+    {
+        //hardcoded, replace with below
+        _colPaddle1.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        _colPaddle2.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        PaddleWest.transform.position = new Vector3(-7, 0);
+        PaddleEast.transform.position = new Vector3( 7, 0);
+
+
+        //use soft-coded approach below instead,... but cyclic dependency needs resolving, as Paddle <--> PongManager's Awake... sigh.
+        
+        //_paddleWestCopy = Instantiate<Paddle>(PaddleWest);
+        //_paddleWestCopy.gameObject.SetActive(true);
+        //_paddleWestCopy.transform.parent = transform;
+        //_paddleWestCopy.name = PaddleWest.name;
+
+        //_paddleEastCopy = Instantiate<Paddle>(PaddleEast);
+        //_paddleEastCopy.gameObject.SetActive(true);
+        //_paddleEastCopy.transform.parent = transform;
+        //_paddleEastCopy.name = PaddleEast.name;
+
     }
 
     void CheckWinner()
@@ -117,6 +150,7 @@ public class PongManager : TSEnvironment
             Debug.Log("Player 1 wins!");
             Score1 = 0;
             Score2 = 0;
+            ResetPaddles();
         }
 
         if (Score2 > 11)
@@ -124,6 +158,7 @@ public class PongManager : TSEnvironment
             Debug.Log("Player 2 wins!");
             Score1 = 0;
             Score2 = 0;
+            ResetPaddles();
         }
     }
 }
